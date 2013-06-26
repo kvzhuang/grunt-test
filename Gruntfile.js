897ef8b280c9e276b1ba74b790b71ee2f9d324c3
Kevin Zhuang <kvzhuang@gmail.com>
2013-06-26 14:59:05 +0800

    Add some sample.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,17 @@ module.exports = function(grunt) {
       files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
         // options here to override JSHint defaults
+        "curly": true,
+        "eqeqeq": true,
+        "immed": true,
+        "latedef": true,
+        "newcap": true,
+        "noarg": true,
+        "sub": true,
+        "undef": true,
+        "boss": true,
+        "eqnull": true,
+        "node": true,
         globals: {
           jQuery: true,
           console: true,
--- src/sample.js
+++ src/sample.js
@@ -1,3 +1,4 @@
+"use strict";
 var Person = {};
 
 Person.prototype.name = "Kevin";
@@ -6,6 +7,13 @@ Person.prototype.age = 10;
 Person.prototype.getName = function () {
     return this.name;
 };
+
+Person.prototype.setName = function (newName) {
+    if (newName !== "") {
+        this.name = newName;
+    }
+};
+
 
 var p = new Person();
 var p = new Person();
